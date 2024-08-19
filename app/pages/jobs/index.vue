75107b829e8af2aaef67f928761e785985feb293
niefang2017 <nie4161204@qq.com>
2024-08-19 21:14:38 +0800

    ✅ feat: 新增加入我们

--- app/pages/jobs/index.vue
+++ app/pages/jobs/index.vue
@@ -14,20 +14,46 @@
   </div>
   <section class="px-4 2xl:px-0 my-8 sm:my-12 md:my-16 xl:my-20">
     <div class="max-w-main m-auto ">
-      <div class="flex flex-col-reverse sm:flex-row justify-center items-center gap-10">
-        <div class="w-full sm:w-1/2">
-          <h2 class="font-bold text-4xl">{{ JobsLang[locale]['jobs']['section1']['title'] }}</h2>
-          <div>
-            <div class="li-square square-blue flex mt-4 " v-for="(item, index) in JobsLang[locale]['jobs']['section1']['list']" :key="index">
-              {{ item }}
-            </div>
+      <div class="flex flex-col-reverse lg:flex-row justify-center items-center gap-10">
+        <div class="w-full lg:w-1/2">
+          <h2 class="font-bold text-4xl mb-8">{{ JobsLang[locale]['jobs']['section1']['title'] }}</h2>
+          <div class="before:block before:shrink-0 before:w-[0.38rem] before:h-[0.38rem] before:mt-2 before:mr-2 before:-inset-1 before:bg-primary-blue flex mt-4" v-for="(item, index) in JobsLang[locale]['jobs']['section1']['list']" :key="index">
+            {{ item }}
           </div>
+          <NuxtLink :to="link" class="w-40 h-10 text-sm leading-10 border border-solid text-center font-bold inline-block box-border mt-9 hover:bg-black dark:hover:bg-white hover:text-white dark:hover:text-black transition delay-150 duration-300 ease-in-out">{{ title }}</NuxtLink>
         </div>
-        <div class="w-full sm:w-1/2">
+        <div class="w-full lg:w-1/2">
           <img :src="imgCard0" :alt="JobsLang[locale]['jobs']['section1']['title']" class="w-full object-cover block m-auto" />
         </div>
       </div>
-      <NuxtLink to="/live" class="w-40 h-10 text-sm leading-10 border border-solid text-center font-bold inline-block box-border mt-9 hover:bg-black dark:hover:bg-white hover:text-white dark:hover:text-black transition delay-150 duration-300 ease-in-out">{{ title }}</NuxtLink>
+    </div>
+  </section>
+  <section class="px-4 2xl:px-0 my-8 sm:my-12 md:my-16 xl:my-20 bg-no-repeat bg-cover bg-center h-[37.5rem] m-auto before:block before:absolute before:-inset-1 before:bg-opacity-90 before:bg-primary-blue relative flex flex-col items-center overflow-hidden" :style="{'background-image': `url(${banner2})`}">
+    <div class="w-full max-w-main m-auto relative z-10 ">
+      <h2 class="font-bold text-4xl mb-8">{{ JobsLang[locale]['jobs']['section2']['title'] }}</h2>
+      <div class="before:block before:shrink-0 before:w-[0.38rem] before:h-[0.38rem] before:mt-2 before:mr-2 before:-inset-1 before:bg-primary-yelleow flex mt-4 relative " v-for="(item, index) in JobsLang[locale]['jobs']['section2']['list']" :key="index">
+        {{ item }}
+      </div>
+      <NuxtLink :to="link" class="w-40 h-10 text-sm leading-10 border border-solid text-center font-bold inline-block box-border mt-9 hover:bg-black dark:hover:bg-white hover:text-white dark:hover:text-black transition delay-150 duration-300 ease-in-out">{{ title }}</NuxtLink>
+
+    </div>
+  </section>
+  <section class="px-4 2xl:px-0 my-8 sm:my-12 md:my-16 xl:my-20">
+    <div class="max-w-main m-auto ">
+      <h2 class="font-bold text-4xl mb-8">{{ JobsLang[locale]['jobs']['section3']['title'] }}</h2>
+      <div class="flex-row lg:flex justify-center gap-5">
+        <div class="flex-1" v-for="(item, index) in JobsLang[locale]['jobs']['section3']['list']" :key="index">
+          <div class="w-full relative overflow-hidden rounded-lg">
+            <img :src="imgCards[index]" :alt="JobsLang[locale]['jobs']['section1']['title']" class="rounded-lg w-full object-cover block m-auto hover:scale-110 transition delay-150 duration-300 ease-in-out" />
+          </div>
+          <div class="p-5 text-black dark:text-white">
+            {{ item }}
+          </div>
+        </div>
+      </div>
+      <div class="text-center">
+        <NuxtLink :to="link" class="w-40 h-10  mt-9 mx-auto text-sm leading-10 border border-solid text-center font-bold inline-block box-border hover:bg-black dark:hover:bg-white hover:text-white dark:hover:text-black transition delay-150 duration-300 ease-in-out">{{ title }}</NuxtLink>
+      </div>
     </div>
   </section>
 
@@ -35,6 +61,7 @@
 <script setup lang="ts">
 import { useDebounceFn } from '@vueuse/core';
 import banner from '~/assets/images/banner_6.jpg'
+import banner2 from '~/assets/images/banner_7.jpg'
 import imgCard0 from '~/assets/images/jobs_card_0.jpg'
 import imgCard1 from '~/assets/images/jobs_card_1.jpg'
 import imgCard2 from '~/assets/images/jobs_card_2.jpg'
@@ -42,8 +69,9 @@ import imgCard3 from '~/assets/images/jobs_card_3.jpg'
 import lang from 'locales/live'
 import JobsLang from 'locales/jobs'
 const { locale } = useI18n()
-
+const link = ref('https://www.risenenergy.com/contact/join_list?lx=1')
 const title = lang[locale.value]['links:jobs'];
+const imgCards = [imgCard1, imgCard2, imgCard3]
 const loading = ref(true)
 setTimeout(() => {
   loading.value = false
