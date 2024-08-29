f4cbd9ee59452fdf3bfed65815f48025ce1c7842
niefang2017 <nie4161204@qq.com>
2024-08-29 23:54:45 +0800

    fix: empty

--- app/components/Empty.vue
+++ app/components/Empty.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col justify-center items-center w-full h-full m-auto">
+  <div class="flex flex-col justify-center items-center w-full h-full m-auto min-h-[220px]">
     <img :src="img || info.img" class="max-w-full block m-auto min-h-1" />
     <p class="mt-4 2xl:mt-6 text-lg xl:text-xl  2xl:text-2xl text-gray-400">{{ text || info.text }}</p>
   </div>
