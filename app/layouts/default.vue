6cdf17dbd09041e863d86806430a4a65046ff78f
wucl05 <308861245@qq.com>
2024-08-30 13:42:42 +0800

    update

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,21 +1,9 @@
 <script setup lang="ts">
-import type { ParsedContent } from '@nuxt/content'
-const route = useRoute()
-const { data: navigation } = await useAsyncData('navigation', () => fetchContentNavigation(), { default: () => [] })
-const { data: files } = useLazyFetch<ParsedContent[]>('/api/search.json', { default: () => [], server: false })
-
-provide('navigation', navigation)
-const loading = ref(false)
-const loadHandler = () => {
-  console.log('加载成功啦')
-  loading.value = true
-}
 </script>
-
 <template>
   <div ref="el" class="app">
-    <AppHeader/>
-      <slot />
-    <PageLoading v-if="!loading && route.name === 'index'" @success="loadHandler" />
+    <AppHeader />
+    <slot />
+    <AppFooter />
   </div>
 </template>
