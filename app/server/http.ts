f3da393d8952e46e4cde52ac176f65781b1b54d0
wucl05 <308861245@qq.com>
2024-08-30 13:39:34 +0800

    接口响应式

--- app/server/http.ts
+++ app/server/http.ts
@@ -2,30 +2,30 @@ import { useFetch, useRuntimeConfig } from "nuxt/app";
 import type { UseFetchOptions } from 'nuxt/app';
 import { hash } from 'ohash';
 
-const fetch = (url: string, options:UseFetchOptions<T>={}, headers: Record<string, string> = {}, handleError: any) => {
+const fetch = (url: string, options: UseFetchOptions<T> = {}, headers: Record<string, string> = {}, handleError: any) => {
   const {
-    public: { apiBase,lang },
+    public: { apiBase, lang },
   } = useRuntimeConfig(); // 3.0正式版环境变量要从useRuntimeConfig里的public拿
-  const reqUrl:string = apiBase + url; // 你的接口地址
+  const reqUrl: string = apiBase + url; // 你的接口地址
 
   // 不设置key，始终拿到的都是第一个请求的值，参数一样则不会进行第二次请求
   const key = hash(JSON.stringify(options) + url);
 
   // 可以设置默认headers例如
   const { $i18n } = useNuxtApp()
-  const locale = $i18n.locale.value
+  // const locale = $i18n.locale.value
+  const locale = $i18n.locale //语言响应式监听
   const customHeaders = {
     'Content-Type': 'application/json;charset=UTF-8',
-    'language': locale ||lang,
+    'language': locale || lang,
     ...headers
   };
-
   return new Promise((resolve, reject) => {
     useFetch(reqUrl, { ...options, key, headers: customHeaders })
-      .then(({ data, error }) => {
+      .then(({ data, error, refresh }) => {
         if (error.value) {
-          console.log('error.value',error.value)
-          const { code=null, msg = '' } = error.value?.data ?? {};
+          console.log('error.value', error.value)
+          const { code = null, msg = '' } = error.value?.data ?? {};
           if (!handleError && code !== 0 && process.client) {
             console.log(msg || '服务异常')
           }
@@ -43,7 +43,7 @@ const fetch = (url: string, options:UseFetchOptions<T>={}, headers: Record<strin
           if (!handleError && value.code !== 0 && process.client) {
             console.log(value.msg || '服务异常')
           }
-          resolve(value);
+          resolve({ ...value, refData: data, refresh });
         }
       })
       .catch((err) => {
@@ -55,24 +55,24 @@ const fetch = (url: string, options:UseFetchOptions<T>={}, headers: Record<strin
   });
 };
 export default class Http {
-  get(url:string, params:Record<string, any>= {}, headers:Record<string, string> ={}, handleError:boolean = false) {
-    return fetch(url, { method: 'get', params }, headers, handleError);
+  get(url: string, params: Record<string, any> = {}, headers: Record<string, string> = {}, handleError: boolean = false, options = {}) {
+    return fetch(url, { method: 'get', params, ...options }, headers, handleError);
   }
 
-  post(url:string, params:Record<string, any> = {}, headers:Record<string, string> ={}, handleError:boolean = false) {
+  post(url: string, params: Record<string, any> = {}, headers: Record<string, string> = {}, handleError: boolean = false, options = {}) {
     return fetch(
       url,
-      { method: 'post', body: { ...params } },
+      { method: 'post', ...options, body: { ...params } },
       headers,
-      handleError,
+      handleError
     );
   }
 
-  put(url:string, params:Record<string, any> = {}, headers:Record<string, string> ={}, handleError:boolean = false) {
+  put(url: string, params: Record<string, any> = {}, headers: Record<string, string> = {}, handleError: boolean = false) {
     return fetch(url, { method: 'put', params }, headers, handleError);
   }
 
-  delete(url:string, params:Record<string, any> = {}, headers:Record<string, string> ={}, handleError:boolean = false) {
+  delete(url: string, params: Record<string, any> = {}, headers: Record<string, string> = {}, handleError: boolean = false) {
     return fetch(url, { method: 'delete', params }, headers, handleError);
   }
 }
