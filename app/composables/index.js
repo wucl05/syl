48a2267efd5c5db4bbc4100a0b10e99820c36b19
wucl05 <308861245@qq.com>
2024-08-30 14:18:25 +0800

    http

--- app/composables/index.js
+++ app/composables/index.js
@@ -1,4 +1,6 @@
 import api from '@/server/api/index';
+import http from '@/server/http';
 // import store from './store/index';
 export const useApi = () => api;
+export const useHttp = () => new http()
 // export const useStore = store;
